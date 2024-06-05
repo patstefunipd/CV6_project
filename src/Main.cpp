9e14ce73ae1e6df41fde210d70f09a04cd0200c2
patstefaniunipd <patrizia.stefani@studenti.unipd.it>
2024-06-05 19:41:42 +0200

    Fix project structure

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,12 +4,10 @@
 using namespace cv;
 using namespace std;
 
-
-
 int main(int argc, char* argv[])
 {
     string filename = "videos/game1_clip1.mp4";
-    if (argc > 0) filename = argv[0];
+    if (argc > 0) filename = argv[1];
     VideoCapture capture(filename);
     Mat frame;
 
