47ddc6f6c59e8cd0b4663c22f2c5455f31d7281b
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-07-11 17:09:03 +0200

    ADD path of video from argument

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,26 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include "VideoHelpers.h"
-using namespace cv;
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    string filename = "videos/game1_clip1.mp4";
-    if (argc > 0) filename = argv[1];
-    VideoCapture capture(filename);
-    Mat frame;
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <video_file_path>" << endl;
+        return -1;
+    }
 
-    if (!capture.isOpened())
-        throw "Error when reading " + filename;
+    string filename = argv[1];
+    cv::VideoCapture capture(filename);
+    cv::Mat frame;
+
+    if (!capture.isOpened()) {
+        cerr << "Error: Could not open video file " << filename << endl;
+        return -1;
+    }
 
     VideoHelpers::showVideo(filename, capture, frame);
+
     return 0;
 }
