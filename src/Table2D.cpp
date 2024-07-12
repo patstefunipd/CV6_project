2f815131ea539347288850ac75b8449c6958630b
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-07-12 09:43:11 +0200

    update table shape

--- src/Table2D.cpp
+++ src/Table2D.cpp
@@ -2,7 +2,7 @@
 
 
 // Initialize static members
-int Table2D::width = 280; 
+int Table2D::width = 540; 
 int Table2D::height = 560; 
 
 int width = 280, height = 560;
@@ -12,11 +12,9 @@ Mat Table2D::creatTable() {
     
     img.setTo(Scalar(10, 180, 0)); // BGR color
 
-    circle(img, Point(width / 2, height / 5), width / 6, Scalar(50, 255, 50), FILLED);
 
     rectangle(img, Point(0, height / 5), Point(width, height), Scalar(10, 180, 0), FILLED);
 
-    line(img, Point(0, height / 5), Point(width, height / 5), Scalar(50, 255, 50));
 
     cvtColor(img, img, COLOR_BGR2RGB);
 
